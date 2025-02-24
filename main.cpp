ef59bc1ef978c0c572647e95383be741d15bf561
Alan Urbanek <aourbanek@mail.bradley.edu>
2025-02-24 11:54:13 -0600

    Modified School struct to resemble and replace Node struct; commented out Node struct

--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ struct Node
 };
 
 template<typename T>
+struct School
+{
+    string name;
+    string address;
+    string city;
+    string state;
+    string county;
+    School* next;
+    School(string val1, string val2, string val3, string val4, string val5) : name(val1), address(val2), city(val3), state(val4), county(val5), next(nullptr) {}
+};
+
+template<typename T>
 class SinglyLinkedList
 {
     Node<T>* head;
@@ -72,15 +84,4 @@ public:
         file.close();
         return data;
     }
-};
-
-template<typename T>
-struct School
-{
-    string name;
-    string address;
-    string city;
-    string state;
-    string county;
-    Node<T>* next;
 };
