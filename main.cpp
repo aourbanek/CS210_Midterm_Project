71a071160d3d59b284f5bb6122cf152f578c2253
Alan Urbanek <aourbanek@mail.bradley.edu>
2025-02-24 11:47:05 -0600

    Moved sll.cpp code into main.cpp and removed reference to sll.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,52 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "sll.cpp"
 using namespace std;
+
+template<typename T>
+struct Node
+{
+    T data;
+    Node* next;
+    Node(T val) : data(val), next(nullptr) {}
+};
+
+template<typename T>
+class SinglyLinkedList
+{
+    Node<T>* head;
+
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void append(T data)
+    {
+        Node<T>* newNode = new Node<T>(data);
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T>* temp = head;
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void printList()
+    {
+        Node<T>* temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+};
 
 class CSVReader {
 public:
